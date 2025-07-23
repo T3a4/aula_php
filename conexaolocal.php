9a677e17c9729de49d3320cd10e91ce07da1f76a
T3a4 <tabatha.mfrsouza@senacsp.edu.br>
2025-07-23 09:01:14 -0300

    atualiza o arquivo

---
+++ conexaolocal.php
@@ -0,0 +1,30 @@
+<?php
+class BancoDeDados {
+  private $host = "localhost";
+  private $nome_banco = "aula_php";
+  private $usuario = "root";
+  private $senha = "";
+
+  public function obterConexao() {
+    $this->conexao = null;
+    try {
+      $dsn = "mysql:host={$this->host};dbname={$this->nome_banco};charset=utf8";
+      
+      $this->conexao = new PDO(
+        $dsn,
+        $this->usuario,
+        $this->senha
+      );
+
+    
+      $this->conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    } catch (PDOException $e) {
+      echo "Erro de conexão: " . $e->getMessage();
+      return null;
+    }
+
+    return $this->conexao;
+  }
+}
+?>
