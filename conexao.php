789af5634bae9ae043b854e26f2d0fdc3936d2bf
T3a4 <tabatha.mfrsouza@senacsp.edu.br>
2025-07-23 09:10:18 -0300

    atualiza o arquivo

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php
 class BancoDeDados {
-  private $host = "localhost";
+  private $hostname = "sql205.infinityfree.com";
   private $nome_banco = "aula_php";
-  private $usuario = "root";
-  private $senha = "";
+  private $usuario = "if0_39541530";
+  private $senha = "Shakyra27";
   private $conexao;
 
   public function obterConexao() {
